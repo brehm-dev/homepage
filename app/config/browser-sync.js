69b7a55ffe2c35b681b09179734211c3911b4120
Andreas Brehm <andreasbrehm86@gmail.com>
2017-12-10 21:12:10 +0100

    creating responsive navbar

--- app/config/browser-sync.js
+++ app/config/browser-sync.js
@@ -2,7 +2,8 @@ module.exports = {
     port: 8080,
     proxy: 'http://localhost:8000',
     files: [
-        '**/**/**/*.**'
+        './app/**/**/**/*.**',
+        './public/**/**/**/*.**',
     ],
     injectChanges: true,
     notify: false,
--- server.js
+++ server.js
@@ -2,14 +2,15 @@
 // packages
 let express = require('express');
 let app = express();
-let bootstrap = require('jquery');
-// console.log('node_modules/');
+
 // app setup
 app.set('view engine', 'pug');
 app.set('views', './app/views/');
-app.use('/images', express.static('./app/images'));
+
+app.use('/images', express.static('./public/images'));
+app.use('/js', express.static('./public/js'))
+app.use('/css', express.static('./public/css'))
 app.use('/jquery', express.static('./node_modules/jquery/dist/'));
-app.use('/bootstrap', express.static('./node_modules/bootstrap/dist/'));
 
 // set routes
 // app.use('/', express.static('./app/pub/'));
