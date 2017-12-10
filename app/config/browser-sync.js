ff43f14e2b3be8daecfe8dc6fc085eff9feafecf
Andreas Brehm <andreasbrehm86@gmail.com>
2017-12-10 15:22:36 +0100

    added some images

--- app/config/browser-sync.js
+++ app/config/browser-sync.js
@@ -2,7 +2,7 @@ module.exports = {
     port: 8080,
     proxy: 'http://localhost:8000',
     files: [
-        '../**/**/**/*.**'
+        '**/**/**/*.**'
     ],
     injectChanges: true,
     notify: false,
