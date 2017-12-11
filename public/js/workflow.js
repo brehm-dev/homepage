8e533768ada57300cc927339be20bd2cdcf84c29
Andreas Brehm <andreasbrehm86@gmail.com>
2017-12-11 21:55:19 +0100

    optimizing topbar offset

--- public/js/workflow.js
+++ public/js/workflow.js
@@ -6,7 +6,7 @@
     let navList = $('.ab-navbar-list'),
     navHeight = navList.height(),
     activeButtonClass = 'ab-navbar-menu-active-button';
-
+    console.log(navHeight)
     function slideNavDown() {
         for (let i = 1; i <= navHeight; i++) {
             let pos = navList.position().top;
@@ -20,17 +20,45 @@
         }
     }
 
+
+    $.fn.extend({
+        class: function() {
+            return 'ab-navbar-menu-active-button';
+        },
+        activate: function() {
+            let cls = this.class()
+            if (!this.hasClass(cls)) {
+                this.addClass(this.class())
+                this.data('activity', true)
+                slideNavDown()
+            }
+        },
+        deactivate: function() {
+            let cls = this.class()
+            if (this.hasClass(cls)) {
+                this.removeClass('ab-navbar-menu-active-button');
+                this.data('activity', false)
+                slideNavUp()
+            }
+
+        }
+    });
+
     // show animation
+
     $('#menu-button').on('click', function(event) {
+        let navLinks = navList.find('.ab-navbar-group-item');
         let $trigger = $(this);
+
+
+
+        navLinks.on('click', function() {
+            $trigger.deactivate()
+        });
         if ($trigger.data('activity')) {
-            $trigger.removeClass(activeButtonClass)
-            $trigger.data('activity', false)
-            slideNavUp()
+            $trigger.deactivate()
         } else {
-            $trigger.addClass(activeButtonClass);
-            $trigger.data('activity', true)
-            slideNavDown()
+            $trigger.activate()
         }
     });
 })($);
