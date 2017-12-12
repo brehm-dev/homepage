9acb6878608299dfa3fa13484fb962c78bd263fb
Andreas Brehm <andreasbrehm86@gmail.com>
2017-12-12 20:05:20 +0100

    cleanup. reached alpha version 0.1.0

--- public/js/workflow.js
+++ public/js/workflow.js
@@ -1,49 +1,105 @@
 "use strict"
 class SideFixer {
     constructor() {
-        this.body = $(document.body);
-        this.header = this.body.find('header');
-        this.navbar = this.body.find('nav').children('.ab-navbar-menu-bar');
-        this.navlist = this.navbar.find('.ab-navbar-list')
-        let h = {
-            list: this.navlist.height(),
-            bar: this.navbar.height()
+        let body = $(document.body),
+        header = body.find('header'),
+        navbar = body.find('nav').children('.ab-navbar-menu-bar'),
+        navlist = navbar.find('.ab-navbar-list'),
+        menuButton = header.find('.ab-navbar-menu-button'),
+        navitems = navlist.find('.ab-navbar-group-item');
+
+        this.elements = {
+            body: body,
+            header: header,
+            nav: {
+                list: navlist,
+                bar: navbar,
+                items: navitems
+            }
         }
-        if (h.list > h.bar) {
-            this.navlist.css('top', '-' + (h.list - h.bar) + 'px')
+
+        this.resolutions = {
+            list: {
+                h: navlist.height(),
+                w: navlist.width()
+            },
+            bar: {
+                h: navbar.height(),
+                w: navbar.width()
+            }
+        }
+
+        if (this.resolutions.list.h > this.resolutions.bar.h) {
+            this.elements.nav.list.css('top', '-' + (this.resolutions.list.h - this.resolutions.bar.h) + 'px')
         }
     }
 
     scrollTo(elem) {
         let elemPos = $(elem).position()
-        let navH = this.navbar.height();
         $('html, body').animate({
-            scrollTop: (elemPos.top - navH)
+            scrollTop: (elemPos.top - this.resolutions.bar.h)
         }, 400);
-        console.log(this)
+    }
+
+    slideNavDown() {
+        this.slideNav(true);
+    }
+
+    slideNavUp() {
+        this.slideNav(false);
+    }
+
+    slideNav(addition) {
+        for (let i = 1; i <= this.resolutions.list.h; i++) {
+            let pos = this.elements.nav.list.position().top;
+            if (addition) {
+                this.elements.nav.list.css('top', pos + 1);
+            } else {
+                this.elements.nav.list.css('top', pos - 1);
+            }
+        }
     }
 }
 
 ;(function() {
-    /**
-     * menu button
-     */
-    let navList = $('.ab-navbar-list'),
-    navHeight = navList.height(),
-    activeButtonClass = 'ab-navbar-menu-active-button';
     let sf = new SideFixer();
-    function slideNavDown() {
-        for (let i = 1; i <= navHeight; i++) {
-            let pos = navList.position().top;
-            navList.css('top', pos + 1);
+
+    $.fn.extend({
+            class: function() {
+                return 'ab-navbar-menu-active-button';
+            },
+            activate: function() {
+                let cls = this.class()
+                if (!this.hasClass(cls)) {
+                    this.addClass(cls)
+                    this.data('activity', true)
+                }
+            },
+            deactivate: function() {
+                let cls = this.class()
+                if (this.hasClass(cls)) {
+                    this.removeClass(cls);
+                    this.data('activity', false)
+                }
+            }
+        });
+
+    $('#menu-button').on('click', function(event) {
+        let $btn = $(this);
+
+        sf.elements.nav.items.on('click', function() {
+            $btn.deactivate();
+            sf.slideNavUp()
+        })
+
+        if ($btn.data('activity')) {
+            $btn.deactivate()
+            sf.slideNavUp()
+        } else {
+            $btn.activate()
+            sf.slideNavDown()
         }
-    }
-    function slideNavUp() {
-        for (let i = 1; i <= navHeight; i++) {
-            let pos = navList.position().top;
-            navList.css('top', pos - 1);
-        }
-    }
+    })
 
     $('a').on('click', function(event) {
         if (this.href.indexOf(location.href) !== 0) {
@@ -52,48 +108,6 @@ class SideFixer {
             event.preventDefault()
             let anchorPos = $(this.attributes.href.value).position()
             sf.scrollTo(this.attributes.href.value)
-
         }
     })
-
-    $.fn.extend({
-        class: function() {
-            return 'ab-navbar-menu-active-button';
-        },
-        activate: function() {
-            let cls = this.class()
-            if (!this.hasClass(cls)) {
-                this.addClass(this.class())
-                this.data('activity', true)
-                slideNavDown()
-            }
-        },
-        deactivate: function() {
-            let cls = this.class()
-            if (this.hasClass(cls)) {
-                this.removeClass('ab-navbar-menu-active-button');
-                this.data('activity', false)
-                slideNavUp()
-            }
-
-        }
-    });
-
-    // show animation
-
-    $('#menu-button').on('click', function(event) {
-        let navLinks = navList.find('.ab-navbar-group-item');
-        let $trigger = $(this);
-
-
-
-        navLinks.on('click', function() {
-            $trigger.deactivate()
-        });
-        if ($trigger.data('activity')) {
-            $trigger.deactivate()
-        } else {
-            $trigger.activate()
-        }
-    });
 })($);
