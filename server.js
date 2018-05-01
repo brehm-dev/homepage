8f356586d87b1fb6ad3283dfbf765f56e0fcadf5
Andreas Brehm <andreasbrehm86@gmail.com>
2018-05-01 20:45:56 +0200

    fixed gitignore

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use('/css', express.static('./public/css'))
 app.use('/jquery', express.static('./node_modules/jquery/dist/'));
 
 // set routes
-// app.use('/', express.static('./app/pub/'));
+
 app.use('/', function (req, res) {
     res.render('index');
 });
